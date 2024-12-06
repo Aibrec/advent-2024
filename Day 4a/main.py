89ca9ecbc79f544f688d8b81f2076f7551ff2bdd
James <jamesmcneice@gmail.com>
2024-12-05 20:38:57 -0700

    Faster

--- Day 4a/main.py
+++ Day 4a/main.py
@@ -3,17 +3,64 @@ import re
 
 file_path = 'input.txt'
 
-def search_diagonal(x_unit, y_unit, x, y, word_search, line_length, num_lines):
-    word = 'XMAS'
-    for i in range(4):
-        next_x = x + i*x_unit
-        next_y = y + i*y_unit
-        if not((0 <= next_x < line_length) and (0 <= next_y < num_lines)):
-            return 0
-        elif word_search[next_y][next_x] != word[i]:
-            return 0
-    else:
-        return 1
+def check_for_xmas(y, x, word_search, line_length, num_lines):
+    sum = 0
+
+    # Check forwards
+    if x+3 < line_length and word_search[y][x:x+4] == 'XMAS':
+        sum += 1
+
+    # Check backwards
+    if x >= 3 and word_search[y][x-3:x+1] == 'SAMX':
+        sum += 1
+
+    # Check up
+    if y >= 3 and (word_search[y][x] + word_search[y-1][x] + word_search[y-2][x] + word_search[y-3][x]) == 'XMAS':
+        sum += 1
+
+    # Check down
+    if y+3 < num_lines and (word_search[y][x] + word_search[y+1][x] + word_search[y+2][x] + word_search[y+3][x]) == 'XMAS':
+        sum += 1
+
+    # for yi in (1,-1):
+    #     for xi in (1,-1):
+    #         if 0 <= (x+xi*3) < line_length and 0 <= (y+yi*3) < num_lines:
+    #             if (word_search[y][x] + word_search[y + yi*1][x + xi*1] + word_search[y + yi*2][x + xi*2] + word_search[y + yi*3][x + xi*3]) == 'XMAS':
+    #                 sum += 1
+
+    #
+    # Check diagonal up-left
+    try:
+        if x >= 3 and y >= 3:
+            if (word_search[y][x] + word_search[y-1][x-1] + word_search[y-2][x-2] + word_search[y-3][x-3]) == 'XMAS':
+                sum += 1
+    except IndexError:
+        pass
+
+    # Check diagonal up-right
+    try:
+        if y >= 3:
+            if (word_search[y][x] + word_search[y-1][x+1] + word_search[y-2][x+2] + word_search[y-3][x+3]) == 'XMAS':
+                sum += 1
+    except IndexError:
+        pass
+
+    # Check diagonal down-right
+    try:
+        if (word_search[y][x] + word_search[y+1][x+1] + word_search[y+2][x+2] + word_search[y+3][x+3]) == 'XMAS':
+            sum += 1
+    except IndexError:
+        pass
+
+    # Check diagonal down-left
+    try:
+        if x >= 3:
+            if (word_search[y][x] + word_search[y+1][x-1] + word_search[y+2][x-2] + word_search[y+3][x-3]) == 'XMAS':
+                sum += 1
+    except IndexError:
+        pass
+
+    return sum
 
 start = time.time_ns()
 with open(file_path, 'r') as file:
@@ -25,17 +72,6 @@ with open(file_path, 'r') as file:
     for line in file:
         word_search.append(line.strip())
 
-    for line in word_search:
-        xmas = re.findall('XMAS', line)
-        samx = re.findall('SAMX', line)
-        sum += (len(xmas) + len(samx))
-
-    for x in range(len(word_search[0])):
-        vertical_line = "".join([word_search[y][x] for y in range(len(word_search))])
-        xmas = re.findall('XMAS', vertical_line)
-        samx = re.findall('SAMX', vertical_line)
-        sum += (len(xmas) + len(samx))
-
     line_length = len(word_search[0])
     num_lines = len(word_search)
     for x in range(line_length):
@@ -43,10 +79,7 @@ with open(file_path, 'r') as file:
             if word_search[y][x] != 'X':
                 continue
 
-            sum += search_diagonal(1, 1, x, y, word_search, line_length, num_lines)
-            sum += search_diagonal(-1, 1, x, y, word_search, line_length, num_lines)
-            sum += search_diagonal(1, -1, x, y, word_search, line_length, num_lines)
-            sum += search_diagonal(-1, -1, x, y, word_search, line_length, num_lines)
+            sum += check_for_xmas(y, x, word_search, line_length, num_lines)
 
 end = time.time_ns()
 print(f"sum is {sum}")
