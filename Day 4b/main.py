ffd6a8753d606d220d81e85dcfe3301bf9908800
James <jamesmcneice@gmail.com>
2024-12-05 20:16:14 -0700

    Faster Day 4b, 3002.7

--- Day 4b/main.py
+++ Day 4b/main.py
@@ -1,21 +1,17 @@
 import time
-from collections import defaultdict
 
 file_path = 'input.txt'
 
-locations = defaultdict(int)
-def search_diagonal(x_unit, y_unit, x, y, word_search, line_length, num_lines):
-    word = 'MAS'
-    for i in range(3):
-        next_x = x + i*x_unit
-        next_y = y + i*y_unit
-        if not((0 <= next_x < line_length) and (0 <= next_y < num_lines)):
-            return
-        elif word_search[next_y][next_x] != word[i]:
-            return
-    else:
-        locations[(y+y_unit, x+x_unit)] += 1
-        return
+def check_for_mas(y, x, word_search):
+    try:
+        if y > 0 and x > 0:
+            if {word_search[y - 1][x - 1], word_search[y + 1][x + 1]} == {'M', 'S'}:
+                if {word_search[y - 1][x + 1], word_search[y + 1][x - 1]} == {'M', 'S'}:
+                    return True
+            return False
+    except IndexError:
+        return False
+
 
 start = time.time_ns()
 with open(file_path, 'r') as file:
@@ -23,23 +19,14 @@ with open(file_path, 'r') as file:
     for line in file:
         word_search.append(line.strip())
 
-    line_length = len(word_search[0])
-    num_lines = len(word_search)
-
-    for x in range(line_length):
-        for y in range(num_lines):
-            if word_search[y][x] != 'M':
+    sum = 0
+    for x in range(len(word_search[0])):
+        for y in range(len(word_search)):
+            if word_search[y][x] != 'A':
                 continue
 
-            search_diagonal(1, 1, x, y, word_search, line_length, num_lines)
-            search_diagonal(-1, 1, x, y, word_search, line_length, num_lines)
-            search_diagonal(1, -1, x, y, word_search, line_length, num_lines)
-            search_diagonal(-1, -1, x, y, word_search, line_length, num_lines)
-
-    sum = 0
-    for key,value in locations.items():
-        if value == 2:
-            sum += 1
+            if check_for_mas(y, x, word_search):
+                sum +=1
 
 end = time.time_ns()
 print(f"sum is {sum}")
