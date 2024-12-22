f12b701b2ac352a60cd12235105eb61cfdac4457
James <jamesmcneice@gmail.com>
2024-12-21 23:33:25 -0700

    Faster

--- Day22b/main.py
+++ Day22b/main.py
@@ -1,7 +1,5 @@
 import time
-import functools
 from collections import defaultdict
-import numpy as np
 
 file_path = 'input.txt'
 
@@ -12,22 +10,33 @@ with open(file_path, 'r') as file:
         line = line.strip()
         monkey_secrets.append(int(line))
 
-def score_table_for_secret(secret):
-    last_digits = np.zeros(2001, dtype=np.int8)
-    last_digits[0] = secret % 10
-    for i in range(1, 2001):
-        secret = (secret ^ (secret * 64)) % 16777216
-        secret = (secret ^ (secret // 32)) % 16777216
-        secret = (secret ^ (secret * 2048)) % 16777216
-        last_digits[i] = secret % 10
+def next_secret(secret):
+    secret = (secret ^ (secret * 64)) % 16777216
+    secret = (secret ^ (secret // 32)) % 16777216
+    secret = (secret ^ (secret * 2048)) % 16777216
+    last_digit = secret % 10
+    return secret, last_digit
 
-    differences = np.ediff1d(last_digits)
+def score_table_for_secret(secret):
+    last_four_differences = [None, None, None, None]
     scores = {}
-    for i in range(len(differences)-4):
-        combination = tuple(differences[i:i+4])
-        last_digit_after_combination = last_digits[i+4]
-        if combination not in scores:
-            scores[combination] = int(last_digit_after_combination)
+    previous_last_digit = secret % 10
+
+    for i in range(3):
+        secret, last_digit = next_secret(secret)
+        last_four_differences[i] = last_digit - previous_last_digit
+        previous_last_digit = last_digit
+
+    for i in range(4, 2001):
+        secret, last_digit = next_secret(secret)
+        last_four_differences[3] = last_digit - previous_last_digit
+        if (combination := tuple(last_four_differences)) not in scores:
+            scores[combination] = last_digit
+
+        previous_last_digit = last_digit
+        last_four_differences[0] = last_four_differences[1]
+        last_four_differences[1] = last_four_differences[2]
+        last_four_differences[2] = last_four_differences[3]
 
     return scores
 
